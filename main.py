63ac1114a2715650485e2bf819ce9fa7e7d85f02
Kyle Gaffney <kylebryangaffney@gmail.com>
2024-05-18 09:41:49 -1000

    Update Contact form

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_bootstrap import Bootstrap5
 import smtplib
-import requests
 import os
 
-
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
 bootstrap = Bootstrap5(app)
 
 @app.route('/')
 def home():
     cur_year = datetime.now().year
-
     return render_template("index.html", year=cur_year)
-
 
 @app.route('/about')
 def about():
     cur_year = datetime.now().year
-
     return render_template("about.html", year=cur_year)
-
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
@@ -29,28 +24,33 @@ def contact():
 
     if request.method == 'POST':
         contact_data = request.form
-        name = contact_data["name"]
-        email = contact_data["email"]
-        message = contact_data["message"]
-        full_message = f"{name}\n{email}\n{message}"
+        name = contact_data.get("name")
+        email = contact_data.get("email")
+        message = contact_data.get("message")
 
-        send_gmail = os.environ.get('MAIL_APP_ADDRESS')
-        gmail_password = os.environ.get('MAIL_APP_PW')
-        receive_email = os.environ.get("MAIL_REC_ADDRESS")
-
-        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-            connection.starttls()
-            connection.login(user=send_gmail, password=gmail_password)
-            connection.sendmail(
-                from_addr=send_gmail, 
-                to_addrs=receive_email,
-                msg=f"Subject: New Message\n\n{full_message}"
-                )
-        return render_template("contact.html", msg_sent=True, year=cur_year)
+        if name and email and message:
+            full_message = f"{name}\n{email}\n{message}"
+            send_mail(full_message)
+            return render_template("contact.html", msg_sent=True, year=cur_year)
+        else:
+            flash("Missing information. Please fill out each field.")
+            return redirect(url_for('contact'))
     return render_template("contact.html", msg_sent=False, year=cur_year)
 
+def send_mail(full_message):
+    send_gmail = os.getenv('MAIL_APP_ADDRESS')
+    gmail_password = os.getenv('MAIL_APP_PW')
+    receive_email = os.getenv("MAIL_REC_ADDRESS")
 
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=send_gmail, password=gmail_password)
+        connection.sendmail(
+            from_addr=send_gmail, 
+            to_addrs=receive_email,
+            msg=f"Subject: New Message From Site\n\n{full_message}"
+        )
+    return 'Email sent'
 
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
